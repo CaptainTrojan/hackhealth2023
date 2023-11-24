99022681a5d34b14366ef84971510be6c92a154b
captaintrojan <ct@ct.gov>
2023-11-24 12:22:42 +0100

    earlystopping

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dataloader import ECGDataModule, HDF5ECGDataset
 import argparse
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import EarlyStopping
 
 
 def train_func(config, max_epochs):
@@ -25,13 +26,20 @@ def train_func(config, max_epochs):
         mode='min',
     )
     wandb_logger = WandbLogger(project='hackhealth2023-autoencoder', config=config)
-
+    early_stop_callback = EarlyStopping(
+        monitor='val_loss',
+        min_delta=0.00,
+        patience=3,
+        verbose=False,
+        mode='min'
+    )
+    
     trainer = pl.Trainer(
         devices="auto",
         accelerator="auto",
         enable_progress_bar=True,
         max_epochs=max_epochs,
-        callbacks=[checkpoint_callback],
+        callbacks=[checkpoint_callback, early_stop_callback],
         logger=wandb_logger,
     )
     trainer.fit(model, datamodule=dm)
@@ -42,6 +50,12 @@ if __name__ == '__main__':
     parser.add_argument("--smoke-test", action="store_true")
     
     # add search_space parameters
+    parser.add_argument("--hidden_channels", type=int, default=64)
+    parser.add_argument("--num_layers", type=int, default=5)
+    parser.add_argument("--kernel_size", type=int, default=3)
+    parser.add_argument("--batch_size", type=int, default=256)
+    parser.add_argument("--learning_rate", type=float, default=1e-4)
+    parser.add_argument("--weight_decay", type=float, default=1e-3)
     
     args = parser.parse_args()
     
@@ -53,11 +67,11 @@ if __name__ == '__main__':
         num_samples = 100
     
     search_space = {
-        "hidden_channels": 64,
-        "num_layers": 5,
-        "kernel_size": 3,
-        "batch_size": 256,
-        "learning_rate": 1e-4,
-        "weight_decay": 1e-3,
+        "hidden_channels": args.hidden_channels,
+        "num_layers": args.num_layers,
+        "kernel_size": args.kernel_size,
+        "batch_size": args.batch_size,
+        "learning_rate": args.learning_rate,
+        "weight_decay": args.weight_decay,
     }
     train_func(search_space, max_epochs)
